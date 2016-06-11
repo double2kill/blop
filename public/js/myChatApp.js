18d21bc5679d2bcf1c5f85d089802cb87fb669e5
double2kill <379563000@qq.com>
2016-06-11 14:05:22 +0800

    修改了部分细节展现形式。

--- public/js/myChatApp.js
+++ public/js/myChatApp.js
@@ -4,12 +4,8 @@ var app = angular.module("myChatApp", []);
 app.controller("myChatCtrl", function($scope) {
   // "even", "/images/one.jpg", "洪淑芳", "11:42:23", "我知道"
   $scope.chatLog = [
-    // {is_odd:'odd', pic:'/images/one.jpg', name:'何腾欢', time:'11:42:23', content:'Clean House'},
-    // {is_odd:'even', pic:'/images/one.jpg', name:'洪淑芳', time:'11:43:23', content:'Yellow River'},
-    // {is_odd:'odd', pic:'/images/one.jpg', name:'何腾欢', time:'11:44:23', content:'Cool!'},
-    // {is_odd:'even', pic:'/images/one.jpg', name:'洪淑芳', time:'11:45:23', content:'Nothing'},
-    // {is_odd:'odd', pic:'/images/one.jpg', name:'何腾欢', time:'11:46:23', content:'Really'},
-    // {is_odd:'even', pic:'/images/one.jpg', name:'洪淑芳', time:'11:47:23', content:'Error'},
+    {is_odd:'odd', pic:'/images/one.jpg', name:'何腾欢', time:'11:42:23', content:'Clean House'},
+    {is_odd:'even', pic:'/images/one.jpg', name:'洪淑芳', time:'11:43:23', content:'Yellow River'}
   ];
 
   $scope.myKeyup = function(e){
@@ -23,16 +19,24 @@ app.controller("myChatCtrl", function($scope) {
     var time_now = new Date();
     // 按照chatLog格式构造一条数据形式。
     var sendInfo = {
-      is_odd: 'odd', //是自己发送所以是左边
+      is_odd: 'even', //是自己发送所以是右边
       pic: '/images/one.jpg', 
       name: '何腾欢',
       time: time_now.toLocaleTimeString(), //使用time_now获取当前时间。
-      content: $scope.todoInput
+      content: $scope.todoInput,
     };
 
     $scope.chatLog.push(sendInfo);
 
-    //清空输入框的数据
-    $scope.todoInput = ""
+    // 清空输入框的数据
+    $scope.todoInput = "";
+
+    // 设置srollTop很高，由于异步，所以加了一个轻微的延时
+    setTimeout(
+      function(){
+        document.getElementById("content-box").scrollTop=1000000;
+      },
+      1
+    )
   }
 });
